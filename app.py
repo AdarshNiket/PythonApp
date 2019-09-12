3cd783918c664e505e060b8e383eac4e7f6e1099
AdarshNiket <adarsh.niket@ge.com>
2019-09-12 15:09:12 +0530

    python tutorial with mosh WIP/if-else statements

---
+++ app.py
@@ -0,0 +1,20 @@
+# print('Adarsh Niket')
+# name = 'Adarsh Niket'
+# age = 30
+# is_newpatient = True
+# print(f'{name[-5:]} , {name[:6]}')
+
+# weight = input('What is your weight(pound)? ')
+# print(type(weight))
+# color = input('What is your favourite color? ')
+# print(name + ' likes ' + color)
+# print(float(weight) * 0.453592)
+
+price = 1000000
+is_goodCredit = True
+if is_goodCredit:
+    print('put down 10%')
+    print(price * .1)
+else:
+    print('put down 20%')
+    print(price * .2)
